d5be6a724399488291b88625ca93a1c25d8e7bca
PabloLanero <pablolaneroperez@gmail.com>
2026-03-08 22:37:17 +0100

    Añadido dockerfile + adaptado la direccion ip para la ec2

--- src/stores/Constants.ts
+++ src/stores/Constants.ts
@@ -1,7 +1,7 @@
 import { defineStore } from 'pinia'
 
 export const useConstants = defineStore('constants', () => {
-  const ApiUrl = 'http://localhost:5211'
+  const ApiUrl = 'http://35.153.240.160:5211'
 
   return { ApiUrl }
 })
